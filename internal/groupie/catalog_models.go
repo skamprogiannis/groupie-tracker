76dc7a3502baca74bae47488e12fe382ec88c872
nkountou <nick.kountour@gmail.com>
2026-06-04 21:29:10 +0300

    feat(groupie): add catalog search

--- internal/groupie/catalog_models.go
+++ internal/groupie/catalog_models.go
@@ -20,6 +20,14 @@ type ArtistDetail struct {
 	DatesLocations map[string][]string
 }
 
+type SearchResult struct {
+	ID           int    `json:"id"`
+	Name         string `json:"name"`
+	Image        string `json:"image"`
+	CreationDate int    `json:"creationDate"`
+	FirstAlbum   string `json:"firstAlbum"`
+}
+
 type Catalog struct {
 	artistsByID map[int]ArtistDetail
 	artists     []ArtistDetail
